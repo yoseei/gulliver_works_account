45fa87b94e8c7eff7826e3d2de70efbae03e10ea
Yoshitaka Kai <katashiyoika@gmail.com>
2021-09-08 12:47:18 +0900

    sign-in画面のデザインほぼ完成して、passwordのinputに目を表示させる機能を実装するところ

--- src/scenes/signIn/index.tsx
+++ src/scenes/signIn/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { Link, useHistory } from "react-router-dom";
+import { ScriptSnapshot } from "typescript";
 import { useCurrentAccount } from "../../hooks/useCurrentAccount";
 import styles from "./style.module.scss";
 import { SignInParams, useSignInPresenter } from "./useSignInPresenter";
@@ -20,12 +21,41 @@ const SignInPage = () => {
   };
   return (
     <div className={styles.page}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input name="account.email" ref={register} />
-        <input name="account.password" ref={register} />
-        <button>ログイン</button>
-        <Link to="/">ホームへ</Link>
-      </form>
+      <div className={styles.login_container}>
+        <h1>求職者ログイン</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.emailInput_wrapper}>
+            <p>メールアドレス</p>
+            <input
+              className={styles.input}
+              name="account.email"
+              placeholder="coadmap@mail.com"
+              ref={register}
+              type="email"
+            />
+          </div>
+          <div className={styles.passwordInput_wrapper}>
+            <p>パスワード</p>
+            <input
+              className={styles.input}
+              name="account.password"
+              placeholder="パスワードを入力"
+              ref={register}
+              type="password"
+            />
+          </div>
+          <div className={styles.loginButton_wrapper}>
+            <button>ログイン</button>
+          </div>
+          <div className={styles.passwordLink_wrapper}>
+            <a href="">パスワードを忘れた方はこちら</a>
+          </div>
+          <div className={styles.signUp_wrapper}>
+            <button>新規登録はこちら</button>
+          </div>
+          {/* <Link to="/">ホームへ</Link> */}
+        </form>
+      </div>
     </div>
   );
 };
