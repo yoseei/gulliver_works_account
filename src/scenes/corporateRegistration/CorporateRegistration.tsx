684720631098fe68a88fe5922f522453a2bb037f
yoseei <katashiyoika@gmail.com>
2021-10-20 17:45:25 +0900

    k業登録ページにvalidation入れた

--- src/scenes/corporateRegistration/CorporateRegistration.tsx
+++ src/scenes/corporateRegistration/CorporateRegistration.tsx
@@ -7,9 +7,10 @@ import CircleButton from "../../components/circleButton/CircleButton";
 import { HttpClient } from "../../utilities/axiosInstance";
 import { useForm } from "react-hook-form";
 import { CompanyDataType } from "../../data/company";
+import { ErrorMessage } from "@hookform/error-message";
 
 const CorporateRegistration = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, errors, reset } = useForm();
 
   const handleCreateCompanyInfo = async (data: CompanyDataType) => {
     try {
@@ -18,8 +19,7 @@ const CorporateRegistration = () => {
         url: `http://localhost:3000/companies`,
         data: { ...data },
       });
-      console.log(response);
-      alert("test");
+      reset();
     } catch (err) {
       console.log(err);
     }
@@ -34,20 +34,52 @@ const CorporateRegistration = () => {
         <div className={styles.mainContainer}>
           <h1>企業登録</h1>
 
-          <Input type={"text"} title={"法人名"} name={"name"} ref={register} />
+          <ErrorMessage
+            className={styles.errorMessage}
+            errors={errors}
+            name="name"
+            as="p"
+          />
+          <Input
+            type={"text"}
+            title={"法人名"}
+            name={"name"}
+            ref={register({
+              required: "※法人名を入力してください。",
+            })}
+          />
 
+          <ErrorMessage
+            className={styles.errorMessage}
+            errors={errors}
+            name="nameKana"
+            as="p"
+          />
           <Input
             type={"text"}
             title={"法人名（ふりがな）"}
             name={"nameKana"}
-            ref={register}
+            ref={register({
+              required: "※法人名（ふりがな）を入力してください。",
+            })}
           />
 
+          <ErrorMessage
+            className={styles.errorMessage}
+            errors={errors}
+            name="headOfficeLocation"
+            as="p"
+          />
           <div className={styles.selectBoxContainer}>
             <p>本店所在地</p>
             <div>
               <div className={styles.selectBox}>
-                <select name="headOfficeLocation" ref={register}>
+                <select
+                  name="headOfficeLocation"
+                  ref={register({
+                    required: "※本店所在地を選択してください。",
+                  })}
+                >
                   <option value=""></option>
                   <option value="sample1">サンプル1</option>
                   <option value="sample2">サンプル2</option>
@@ -57,11 +89,22 @@ const CorporateRegistration = () => {
             </div>
           </div>
 
+          <ErrorMessage
+            className={styles.errorMessage}
+            errors={errors}
+            name="yearOfEstablishment"
+            as="p"
+          />
           <div className={styles.selectBoxContainer}>
             <p>設立年</p>
             <div className={styles.selectBoxWrapper}>
               <div className={styles.selectBox}>
-                <select name="yearOfEstablishment" ref={register}>
+                <select
+                  name="yearOfEstablishment"
+                  ref={register({
+                    required: "※設立年を選択してください。",
+                  })}
+                >
                   <option value=""></option>
                   <option value="1">1</option>
                   <option value="2">2</option>
@@ -78,6 +121,7 @@ const CorporateRegistration = () => {
             name={"hpUrl"}
             ref={register}
           />
+
           <Input
             type={"text"}
             title={"電話番号"}
@@ -112,19 +156,36 @@ const CorporateRegistration = () => {
             <p>代表氏名</p>
             <div className={styles.inputContainer}>
               <div className={styles.leftWrapper}>
+                <ErrorMessage
+                  className={styles.errorMessage}
+                  errors={errors}
+                  name="representativeLast"
+                  as="p"
+                />
                 <p>姓</p>
                 <Input
                   type={"text"}
                   name={"representativeLast"}
-                  ref={register}
+                  ref={register({
+                    required: "※姓を入力してください。",
+                  })}
                 />
               </div>
+
               <div className={styles.rightWrapper}>
+                <ErrorMessage
+                  className={styles.errorMessage}
+                  errors={errors}
+                  name="representativeFirst"
+                  as="p"
+                />
                 <p>名</p>
                 <Input
                   type={"text"}
                   name={"representativeFirst"}
-                  ref={register}
+                  ref={register({
+                    required: "※名を入力してください。",
+                  })}
                 />
               </div>
             </div>
@@ -134,19 +195,36 @@ const CorporateRegistration = () => {
             <p>代表氏名（ふりがな）</p>
             <div className={styles.inputContainer}>
               <div className={styles.leftWrapper}>
+                <ErrorMessage
+                  className={styles.errorMessage}
+                  errors={errors}
+                  name="representativeKanaLast"
+                  as="p"
+                />
                 <p>姓</p>
                 <Input
                   type={"text"}
                   name={"representativeKanaLast"}
-                  ref={register}
+                  ref={register({
+                    required: "※姓(ふりがな)を入力してください。",
+                  })}
                 />
               </div>
+
               <div className={styles.rightWrapper}>
+                <ErrorMessage
+                  className={styles.errorMessage}
+                  errors={errors}
+                  name="representativeKanaFirst"
+                  as="p"
+                />
                 <p>名</p>
                 <Input
                   type={"text"}
                   name={"representativeKanaFirst"}
-                  ref={register}
+                  ref={register({
+                    required: "※名(ふりがな)を入力してください。",
+                  })}
                 />
               </div>
             </div>
@@ -158,12 +236,14 @@ const CorporateRegistration = () => {
             name={"netSales"}
             ref={register}
           />
+
           <Input
             type={"text"}
             title={"従業員数"}
             name={"numbersOfEmployees"}
             ref={register}
           />
+
           <Input
             type={"text"}
             title={"平均年齢"}
@@ -171,13 +251,29 @@ const CorporateRegistration = () => {
             ref={register}
           />
 
-          <Textarea rows={3} name={"businessSummery"} title={"事業概要"} />
+          <ErrorMessage
+            className={styles.errorMessage}
+            errors={errors}
+            name="businessSummery"
+            as="p"
+          />
+          <Textarea
+            rows={3}
+            name={"businessSummery"}
+            title={"事業概要"}
+            ref={register({
+              required: "※事業概要を入力してください。",
+            })}
+          />
+
           <Textarea
             name={"corporatePr"}
             placeholder="アカウント作成後でも入力・更新いただけます&#13;&#10;※1. 求人応募の際は、本項目への入力は必須です&#13;&#10;※2. 内容を充実させることで、スカウト受信・選考通過の可能性が高まります"
             rows={4}
             title={"企業PR（５００文字）"}
+            ref={register}
           />
+
           <div className={styles.logoContainer}>
             <label>
               <Input
@@ -196,9 +292,7 @@ const CorporateRegistration = () => {
           </div>
 
           <div className={styles.buttonContainer}>
-            <CircleButton
-            // onClick={() => alert("投稿しました")}
-            />
+            <CircleButton />
           </div>
         </div>
       </form>
