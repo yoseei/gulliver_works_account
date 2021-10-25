e410841bc15852099da570a0f0d7c483d422b974
yoseei <katashiyoika@gmail.com>
2021-10-25 12:46:42 +0900

    微調整

--- src/scenes/companyDetail/CompanyDetail.tsx
+++ src/scenes/companyDetail/CompanyDetail.tsx
@@ -59,7 +59,7 @@ const CompanyDetail = () => {
             </div>
             <div className={styles.rightWrapper}>
               <p>
-                {company?.representativeLast} + {company?.representativeFirst}
+                {company?.representativeLast} {company?.representativeFirst}
               </p>
             </div>
           </div>
