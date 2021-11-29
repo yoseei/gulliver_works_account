c30d3c0b3ee235d37f7213fd3ebd279ddaef96b7
yoseei <katashiyoika@gmail.com>
2021-11-29 16:55:01 +0900

    slickの実装を試す前に一旦commit

--- src/components/recruitCardSwiper/RecruitCardSwiper.tsx
+++ src/components/recruitCardSwiper/RecruitCardSwiper.tsx
@@ -1,47 +1,48 @@
 import React, { useEffect, useState } from "react";
 import styles from "./RecruitCardSwiper.module.scss";
-// #TODO: 次のタスク用import
-// import { HttpClient } from "../../utilities/axiosInstance";
-// import { localHostURL } from "../../hooks/localHostURL";
-// import { RecruitmentDataType } from "../../data/recruitment";
+import { HttpClient } from "../../utilities/axiosInstance";
+import { localHostURL } from "../../hooks/localHostURL";
+import { RecruitmentDataType } from "../../data/recruitment";
 import image from "../image/Ellipse2.png";
 import coverImage from "../image/Rectangle77.png";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 
 const RecruitCardSwiper = () => {
-  // #TODO: 次のタスクにて、allRecruitmentをmapで取得
-  // const [allRecruitment, setAllRecruitment] = useState<RecruitmentDataType[]>();
+  const [allRecruitments, setAllRecruitments] =
+    useState<RecruitmentDataType[]>();
 
-  // useEffect(() => {
-  //   const fetchAllRecruitment = async () => {
-  //     const res = await HttpClient.request({
-  //       method: "GET",
-  //       url: `${localHostURL}/recruitments`,
-  //     });
-  //     setAllRecruitment(res.data);
-  //   };
-  //   fetchAllRecruitment();
-  // }, []);
+  useEffect(() => {
+    const fetchAllRecruitment = async () => {
+      const res = await HttpClient.request({
+        method: "GET",
+        url: `${localHostURL}/recruitments`,
+      });
+      setAllRecruitments(res.data);
+    };
+    fetchAllRecruitment();
+  }, []);
 
-  const recruitmentCards = (
-    <div className={styles.recruitmentContainer}>
-      <div className={styles.coverImageWrapper}>
-        <img src={coverImage} alt="cover画像" />
-      </div>
-
-      <div className={styles.titleWrapper}>
-        <h3>タイトルタイトルタイトルタイトル</h3>
-      </div>
-      <div className={styles.companyNameWrapper}>
-        <div className={styles.imageWrapper}>
-          <img src={image} alt="image画像" />
+  const recruitmentCards = allRecruitments?.map((recruitment) => (
+    <div className={styles.recruitmentsContainer} key={recruitment.id}>
+      <div className={styles.recruitmentContainer}>
+        <div className={styles.coverImageWrapper}>
+          <img src={coverImage} alt="cover画像" />
         </div>
 
-        <p>本文本文本文本文本文本文本文本文本文本文本文</p>
+        <div className={styles.titleWrapper}>
+          <h3>{recruitment.title}</h3>
+        </div>
+        <div className={styles.companyNameWrapper}>
+          <div className={styles.imageWrapper}>
+            <img src={image} alt="image画像" />
+          </div>
+
+          <p>{recruitment.company.name}</p>
+        </div>
       </div>
     </div>
-  );
+  ));
   return (
     <div className={styles.root}>
       <h1>おすすめの募集</h1>
@@ -52,10 +53,9 @@ const RecruitCardSwiper = () => {
         centerSlidePercentage={50}
         infiniteLoop={true}
         showStatus={false}
+        showThumbs={false}
       >
-        <div className={styles.recruitmentsContainer}>{recruitmentCards}</div>
-        <div className={styles.recruitmentsContainer}>{recruitmentCards}</div>
-        <div className={styles.recruitmentsContainer}>{recruitmentCards}</div>
+        {recruitmentCards}
       </Carousel>
     </div>
   );
