eebc64fa1f85107310d47a7764cf969b1fa3504e
yoseei <katashiyoika@gmail.com>
2021-12-07 16:34:45 +0900

    feat: 同じ募集に応募できなくするため、buttonにdisabled機能を実装

--- src/components/companyProfile/CompanyProfile.tsx
+++ src/components/companyProfile/CompanyProfile.tsx
@@ -45,7 +45,6 @@ const CompanyProfile: FC<PropsType> = ({onClick, recruitment}) => {
     alert("応募しました！")
     history.push("/")
   }
-  console.log("hoge")
 
   return (
     <div className={styles.root}>
