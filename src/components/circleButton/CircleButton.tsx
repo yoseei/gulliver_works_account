9a22d0c6d3d7687728be8bd9542acd3e595e60a0
yoseei <katashiyoika@gmail.com>
2021-12-07 17:54:15 +0900

    refactor: changeRequestedの要請があったため、各修正

--- src/components/circleButton/CircleButton.tsx
+++ src/components/circleButton/CircleButton.tsx
@@ -5,24 +5,40 @@ import {useParams} from "react-router-dom";
 
 type PropsType = {
   disabled?: boolean;
-  onClick?: React.MouseEventHandler<HTMLParagraphElement> | undefined;
-  text: "応募する" | "更新" | "作成" | "新規作成";
+  onClick?: React.MouseEventHandler<HTMLParagraphElement>;
+  linkTo?: ReturnType<any>;
+  text: string;
   type: "submit" | "button";
   width?: string;
 };
-const CircleButton: FC<PropsType> = ({disabled, onClick, text, type, width}) => {
-  const { id } = useParams<{ id: string }>();
+const CircleButton: FC<PropsType> = ({disabled, onClick, linkTo, text, type, width}) => {
+  const {id} = useParams<{ id: string }>();
   return (
     <>
-      <Link
-        to={
-          text === "新規作成"
-            ? "/create_recruitment"
-            : "" || text === "更新"
-              ? "/edit_recruitment"
-              : "" || text === "応募する" ? `/applicant_recruitment/${id}/detail` : ""
-        }
-      >
+      {linkTo ?
+        <Link
+          to={
+            text === "新規作成"
+              ? "/create_recruitment"
+              : "" || text === "更新"
+                ? "/edit_recruitment"
+                : ""
+          }
+        >
+          <Button
+            alignItems={"center"}
+            border={"none"}
+            borderRadius={"30px"}
+            color={"primary"}
+            disabled={disabled}
+            fontSize={"1rem"}
+            onClick={onClick}
+            text={text}
+            type={type}
+            width={width}
+          />
+        </Link>
+        :
         <Button
           alignItems={"center"}
           border={"none"}
@@ -35,7 +51,7 @@ const CircleButton: FC<PropsType> = ({disabled, onClick, text, type, width}) => 
           type={type}
           width={width}
         />
-      </Link>
+      }
     </>
   );
 };
